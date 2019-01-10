e36d0287f797f997a3993849546153bdd24f3b16
Mike Harris <MikeMKH@users.noreply.github.com>
2019-01-09 18:22:53 -0600

    reformat

--- ch05/examples/src/lib.rs
+++ ch05/examples/src/lib.rs
@@ -1,40 +1,61 @@
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
     fn it_can_calculate_area_by_function() {
-        let r = Rectangle { width: 2, height: 3 };
+        let r = Rectangle {
+            width: 2,
+            height: 3,
+        };
         assert_eq!(area_rec(&r), 6);
     }
-    
+
     #[test]
     fn it_can_calculate_area_by_method() {
-        let r = Rectangle { width: 2, height: 3 };
+        let r = Rectangle {
+            width: 2,
+            height: 3,
+        };
         assert_eq!(r.area(), 6);
     }
-    
+
     #[test]
     fn it_can_hold_smaller() {
-        let r = Rectangle { width: 2, height: 3 };
-        let smaller = Rectangle { width: 1, height: 1 };
+        let r = Rectangle {
+            width: 2,
+            height: 3,
+        };
+        let smaller = Rectangle {
+            width: 1,
+            height: 1,
+        };
         assert!(r.can_hold(&smaller));
     }
-    
+
     #[test]
     fn it_cannot_hold_larger() {
-        let r = Rectangle { width: 2, height: 3 };
-        let larger = Rectangle { width: 20, height: 30 };
+        let r = Rectangle {
+            width: 2,
+            height: 3,
+        };
+        let larger = Rectangle {
+            width: 20,
+            height: 30,
+        };
         assert!(!r.can_hold(&larger));
     }
-    
+
     #[test]
     fn it_cannot_hold_equal() {
-        let r = Rectangle { width: 2, height: 3 };
+        let r = Rectangle {
+            width: 2,
+            height: 3,
+        };
         let equal = r.clone();
         assert!(!r.can_hold(&equal));
     }
-    
+
     #[test]
     fn it_can_create_squares() {
         let square = Rectangle::square(2);
@@ -42,8 +63,7 @@ mod tests {
     }
 }
 
-#[derive(Debug)]
-#[derive(Copy)]
+#[derive(Debug, Copy)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -57,16 +77,21 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
-    
+
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
-    
+
     fn square(side: u32) -> Rectangle {
-        Rectangle { width: side, height: side }
+        Rectangle {
+            width: side,
+            height: side,
+        }
     }
 }
 
 impl Clone for Rectangle {
-    fn clone(&self) -> Rectangle { *self }
+    fn clone(&self) -> Rectangle {
+        *self
+    }
 }
