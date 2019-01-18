4f36086887a5d998dab032f4617b766da63decea
Mike Harris <MikeMKH@users.noreply.github.com>
2019-01-18 06:33:05 -0600

    add longest from chapter 10

--- ch10/examples/src/lib.rs
+++ ch10/examples/src/lib.rs
@@ -1,23 +1,55 @@
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
     fn it_finds_largest_copy() {
         let list = vec![1, 9, 3];
         assert_eq!(largest_copy(&list), 9);
     }
-    
+
     #[test]
     fn it_finds_largest_clone() {
         let list = vec![1, 9, 3];
         assert_eq!(largest_clone(&list), 9);
     }
-    
+
     #[test]
     fn it_finds_largest_lifetime() {
         let list = vec![1, 9, 3];
         assert_eq!(*largest_lifetime(&list), 9);
+    }
+
+    #[test]
+    fn barrow_checker_example() {
+        let r;
+        {
+            let x = 5;
+            r = &x;
+
+            assert_eq!(*r, 5);
+        } // `x` dropped here while still borrowed
+          // assert_eq!(*r, 5); // borrowed value does not live long enough
+    }
+
+    #[test]
+    fn it_will_find_the_longest() {
+        let long = String::from("this is the longest");
+        let short = "shortest";
+
+        assert_eq!(longest(long.as_str(), short), long.as_str());
+    }
+
+    #[test]
+    fn it_will_find_the_longest_using_the_smaller_scope() {
+        let long;
+        let short = "shortest";
+        {
+            long = String::from("this is the longest");
+
+            assert_eq!(longest(long.as_str(), short), long.as_str());
+        }
+        // assert_eq!(longest(long.as_str(), short), long.as_str()); // not found in this scope
     }
 }
 
@@ -53,4 +85,21 @@ fn largest_lifetime<'a, T: PartialOrd>(list: &'a [T]) -> &'a T {
     }
 
     &list[largest_idx]
+}
+
+use std::cmp::Ordering;
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    match x.len().cmp(&y.len()) {
+        Ordering::Greater => x,
+        Ordering::Equal => x,
+        Ordering::Less => y,
+    }
+    /*
+    -- what normal people would do
+        if x.len() > y.len() {
+            x
+        } else {
+            y
+        }
+    */
 }
