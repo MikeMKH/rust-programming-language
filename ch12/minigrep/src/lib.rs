a8bdf25f45b437b79e10a3c1eb9ed4320acb8e03
Mike Harris <MikeMKH@users.noreply.github.com>
2019-01-24 06:44:03 -0600

    add search

--- ch12/minigrep/src/lib.rs
+++ ch12/minigrep/src/lib.rs
@@ -1,6 +1,41 @@
+use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
-use std::error::Error;
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn parse_two_arguments() {
+        let result = Config::new(&[
+            String::from("app"),
+            String::from("hello"),
+            String::from("file.txt"),
+        ]);
+        assert_eq!(
+            result.unwrap(),
+            Config {
+                query: String::from("hello"),
+                filename: String::from("file.txt")
+            }
+        );
+    }
+
+    #[test]
+    fn search_one_result() {
+        let query = "duct";
+        let content = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, content)
+        );
+    }
+}
 
 #[derive(Debug)]
 pub struct Config {
@@ -21,11 +56,26 @@ impl Config {
     }
 }
 
+impl PartialEq for Config {
+    fn eq(&self, other: &Config) -> bool {
+        self.query == other.query && self.filename == self.filename
+    }
+}
+
+pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    content.lines().filter(|line| {
+        line.contains(query)
+    }).collect()
+}
+
 pub fn run(config: Config) -> Result<(), Box<Error>> {
     let mut f = File::open(config.filename)?;
     let mut content = String::new();
     f.read_to_string(&mut content)?;
 
-    println!("  with text: \n{}", content);
+    for line in search(&config.query, &content) {
+        println!("{}", line);
+    }
+    
     Ok(())
 }
--- ch12/minigrep/src/main.rs
+++ ch12/minigrep/src/main.rs
@@ -8,15 +8,12 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Parsing error on {:?} arguments: {}", args, err);
+        eprintln!("Parsing error on {:?} arguments: {}", args, err);
         process::exit(1);
     });
 
-    println!("Searching for: {}", config.query);
-    println!("  in file: {}", config.filename);
-
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
