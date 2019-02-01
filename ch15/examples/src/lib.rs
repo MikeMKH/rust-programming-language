0ca945760d8e317aac3faf877110639b0cb6dbba
Mike Harris <MikeMKH@users.noreply.github.com>
2019-02-01 06:33:18 -0600

    add more smart pointer examples from chapter 15

--- ch15/examples/src/lib.rs
+++ ch15/examples/src/lib.rs
@@ -1,20 +1,20 @@
 #[cfg(test)]
 mod tests {
-    use crate::List::{Cons, Empty};
     use super::*;
-    
+    use crate::List::{Cons, Empty};
+
     #[test]
     fn it_can_use_the_stack() {
         let x = 5;
         assert_eq!(x, 5);
     }
-    
+
     #[test]
     fn it_can_use_the_heap() {
         let x = Box::new(5);
         assert_eq!(*x, 5);
     }
-    
+
     #[test]
     fn cons_works() {
         let col = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Empty))))));
@@ -23,21 +23,39 @@ mod tests {
             Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Empty))))))
         );
     }
-    
+
     #[test]
     fn follow_pointer() {
         let x = 42;
         let y = &x;
-        
+
         assert_eq!(x, *y);
     }
-    
+
     #[test]
     fn sphere_is_like_box() {
         let x = "Hello";
         let y = Sphere(x);
-        
+        let z = Box::new(x);
+
         assert_eq!(x, *y);
+        assert_eq!(*z, *y);
+    }
+
+    #[test]
+    #[should_panic(expected = "dropping the smarts")]
+    fn fake_it_until_you_make_it() {
+        let x = "Hello";
+        let y = IGotTheSmarts { data: x };
+
+        assert_eq!(x, y.data);
+    }
+
+    #[test]
+    #[should_panic(expected = "dropping the smarts")]
+    fn drop_it_likes_it_hot() {
+        let x = IGotTheSmarts { data: 42 };
+        drop(x);
     }
 }
 
@@ -59,8 +77,19 @@ use std::ops::Deref;
 
 impl<T> Deref for Sphere<T> {
     type Target = T;
-    
+
     fn deref(&self) -> &T {
         &self.0
+    }
+}
+
+#[derive(Debug)]
+struct IGotTheSmarts<T> {
+    data: T,
+}
+
+impl<T> Drop for IGotTheSmarts<T> {
+    fn drop(&mut self) {
+        panic!("dropping the smarts")
     }
 }
