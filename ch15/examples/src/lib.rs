5025230720435bad2cf82c928a1eb4c4858f60c6
Mike Harris <MikeMKH@users.noreply.github.com>
2019-02-04 06:49:56 -0600

    add reference count examples from chapter 15

--- ch15/examples/src/lib.rs
+++ ch15/examples/src/lib.rs
@@ -2,6 +2,12 @@
 mod tests {
     use super::*;
     use crate::List::{Cons, Empty};
+    use crate::RcList::Cons as RcCons;
+    use crate::RcList::Empty as RcEmpty;
+    use std::rc::Rc;
+    use crate::RefList::Cons as RefCons;
+    use crate::RefList::Empty as RefEmpty;
+    use std::cell::RefCell;
 
     #[test]
     fn it_can_use_the_stack() {
@@ -57,11 +63,68 @@ mod tests {
         let x = IGotTheSmarts { data: 42 };
         drop(x);
     }
+    
+    #[test]
+    fn rc_cons_works() {
+        let a = Rc::new(RcCons(5, Rc::new(RcCons(10, Rc::new(RcEmpty)))));
+        
+        assert_eq!(a, Rc::clone(&a));
+    }
+    
+    #[test]
+    fn counting_references() {
+        let a = Rc::new(RcCons(5, Rc::new(RcEmpty)));
+        assert_eq!(1, Rc::strong_count(&a));
+        
+        let b = Rc::new(RcCons(10, Rc::clone(&a)));
+        assert_eq!(2, Rc::strong_count(&a));
+        
+        {
+            let c = Rc::new(RcCons(15, Rc::clone(&a)));
+            assert_eq!(3, Rc::strong_count(&a));
+            
+            let d = Rc::new(RcCons(20, Rc::clone(&a)));
+            assert_eq!(4, Rc::strong_count(&a));
+        }
+        assert_eq!(2, Rc::strong_count(&a));
+    }
+    /*
+    #[test]
+    fn ref_cons_works() {
+        let value = Rc::new(RefCell::new(5));
+        
+        let a = Rc::new(RefCons(Rc::clone(&value), Rc::new(RefEmpty)));
+        let b = Rc::new(RefCons(Rc::new(6), Rc::clone(&a));
+        let c = Rc::new(RefCons(Rc::new(7), Rc::clone(&a));
+        
+        *value.borrow_mut() *= 10;
+        
+        assert_eq!(
+            a,
+            Rc::new(RefCons(Rc::new(50), Rc::new(RefEmpty)))
+        )
+    }*/
 }
 
 #[derive(Debug, PartialEq)]
 enum List<T> {
     Cons(T, Box<List<T>>),
+    Empty,
+}
+
+use std::rc::Rc;
+
+#[derive(Debug, PartialEq)]
+enum RcList<T> {
+    Cons(T, Rc<RcList<T>>),
+    Empty,
+}
+
+use std::cell::RefCell;
+
+#[derive(Debug, PartialEq)]
+enum RefList<T> {
+    Cons(Rc<RefCell<T>>, Rc<RefList<T>>),
     Empty,
 }
 
