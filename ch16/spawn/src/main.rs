cf45d9549056ddec89b6b9326a3c39a6967c7c7d
Mike Harris <MikeMKH@users.noreply.github.com>
2019-02-06 06:18:11 -0600

    refactor join handle example 16-2 from chapter 16

--- ch16/spawn/src/main.rs
+++ ch16/spawn/src/main.rs
@@ -2,7 +2,7 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    thread::spawn(|| {
+    let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("thread: number={}", i);
             thread::sleep(Duration::from_millis(1));
@@ -13,4 +13,6 @@ fn main() {
         println!("main: number={}", i);
         thread::sleep(Duration::from_millis(1));
     }
+    
+    handle.join().unwrap();
 }
