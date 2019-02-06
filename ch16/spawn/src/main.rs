1de529fc81a17e73b01342d43177a39758171fa6
Mike Harris <MikeMKH@users.noreply.github.com>
2019-02-06 06:16:39 -0600

    add spawn example 16-1 from chapter 16

--- ch16/spawn/src/main.rs
+++ ch16/spawn/src/main.rs
@@ -1,3 +1,16 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    thread::spawn(|| {
+        for i in 1..10 {
+            println!("thread: number={}", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+    
+    for i in 1..5 {
+        println!("main: number={}", i);
+        thread::sleep(Duration::from_millis(1));
+    }
 }
