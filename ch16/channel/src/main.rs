6ec59999ce984b737cf08aa2d7ecaae5e27c25c3
Mike Harris <MikeMKH@users.noreply.github.com>
2019-02-07 06:21:41 -0600

    add multiple value example 16-10 from chapter 16

--- ch16/channel/src/main.rs
+++ ch16/channel/src/main.rs
@@ -1,14 +1,28 @@
-use std::thread;
 use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let (tx, rx) = mpsc::channel();
-    
+
     thread::spawn(move || {
-        let message = String::from("We are the Borg. Lower your shields and surrender your ships.");
-        tx.send(message).unwrap();
+        let messages = vec![
+            String::from("We are the Borg."),
+            String::from("Lower your shields and surrender your ships."),
+            String::from(
+                "We will add your biological and technological distinctiveness to our own.",
+            ),
+            String::from("Your culture will adapt to service us."),
+            String::from("Resistance is futile."),
+        ];
+
+        for m in messages {
+            tx.send(m).unwrap();
+            thread::sleep(Duration::from_millis(100));
+        }
     });
-    
-    let message = rx.recv().unwrap();
-    println!("Received: {}", message);
+
+    for received in rx {
+        println!("Received: {}", received);
+    }
 }
