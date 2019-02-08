caaf8f7c1800c6afb8a8b97d158d7a8f49605ce1
Mike Harris <MikeMKH@users.noreply.github.com>
2019-02-08 06:17:24 -0600

    add atomic example 16-15 from chapter 16

--- ch16/mutex/src/main.rs
+++ ch16/mutex/src/main.rs
@@ -1,13 +1,23 @@
-use std::sync::Mutex;
+use std::sync::{Mutex, Arc};
+use std::thread;
 
 fn main() {
-    let m = Mutex::new(42);
-    println!("before {:?}", m);
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
     
-    {
-        let mut num = m.lock().unwrap();
-        *num = 8;
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            
+            *num += 1;
+        });
+        handles.push(handle);
     }
     
-    println!("after  {:?}", m);
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    
+    println!("Result: {:?}", *counter.lock().unwrap());
 }
