9ff4241c113a6d4b7f634e34cad334405f2d349f
Mike Harris <MikeMKH@users.noreply.github.com>
2019-02-28 06:08:06 -0600

    add HTTP 404 response example from chapter 20

--- ch20/hello/src/main.rs
+++ ch20/hello/src/main.rs
@@ -1,13 +1,13 @@
-use std::io::prelude::*;
 use std::fs::File;
+use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        
+
         handle_connection(stream);
     }
 }
@@ -16,13 +16,27 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-    
-    let mut hello = File::open("hello.html").unwrap();
-    
-    let mut contents = String::new();
-    hello.read_to_string(&mut contents).unwrap();
-    
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    if (buffer.starts_with(get)) {
+        let mut hello = File::open("hello.html").unwrap();
+
+        let mut contents = String::new();
+        hello.read_to_string(&mut contents).unwrap();
+
+        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let status = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        let mut error = File::open("404.html").unwrap();
+        
+        let mut contents = String::new();
+        error.read_to_string(&mut contents).unwrap();
+        
+        let response = format!("{}{}", status, contents);
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
